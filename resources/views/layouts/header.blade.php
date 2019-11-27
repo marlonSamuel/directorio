d3a8dcbeea16dfdc6e3c257eb1790159e22d4387
marlonSamuel <marlongf2195@gmail.com>
2019-11-27 13:11:13 -0600

    negrita

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,15 +21,15 @@
 	            <!-- Authentication Links -->
 	            @guest
 	                <li class="nav-item">
-	                    <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
+	                    <a class="nav-link" href="{{ route('login') }}"><b>{{ __('Ingresar') }}</b></a>
 	                </li>
 	            @else
 	            <li class="nav-item">
-	                <a class="nav-link" href="{{ route('usuario') }}">{{ __('Usuarios') }}</a>
+	                <a class="nav-link" href="{{ route('usuario') }}"><b>{{ __('Usuarios') }}</b></a>
 	            </li>
 	            <li class="nav-item dropdown">
 	                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-	                    {{ Auth::user()->empleado->primer_nombre }} {{ Auth::user()->empleado->primer_apellido }}<span class="caret"></span>
+	                  <b>  {{ Auth::user()->empleado->primer_nombre }} {{ Auth::user()->empleado->primer_apellido }} </b><span class="caret"></span>
 	                </a>
 
 	                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
